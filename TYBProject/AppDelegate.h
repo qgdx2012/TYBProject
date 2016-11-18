a67c1455478dcaa2b402e68ac522dbeb3e704894
qgdx2012 <qianlong@qianlongs-macbook-air.local>
2016-11-18 21:07:51 +0800

    Initial Commit

---
+++ TYBProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYBProject
+//
+//  Created by 钱龙 on 16/11/18.
+//  Copyright © 2016年 钱龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYBProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYBProject
+//
+//  Created by 钱龙 on 16/11/18.
+//  Copyright © 2016年 钱龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
